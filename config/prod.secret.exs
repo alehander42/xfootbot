bd09257e0651e0b959329471e77a337bdae1faaa
Alexander Ivanov <alehander42@gmail.com>
2016-06-04 20:55:12 +0300

    Ok

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,7 +4,7 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :xfootbot, Xfootbot.Endpoint,
-  secret_key_base: "E1B3K0q5iYQDb+VzgMdQy30LRfaVawBtFew1jjQiIyz2mpaNDn48k+zZsLOm2pEJ"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :xfootbot, Xfootbot.Repo,
