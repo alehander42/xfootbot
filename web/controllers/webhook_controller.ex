6e9ea8ed61a0fb1ea28f94b6727c9cd10cac4408
Alexander Ivanov <alehander42@gmail.com>
2016-06-07 13:13:00 +0300

    Experiment #2

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -2,9 +2,13 @@
 defmodule Xfootbot.WebHookController do
   use FacebookMessenger.Phoenix.Controller
 
+  require Logger
+  
   def message_received(msg) do
     text = FacebookMessenger.Response.message_texts(msg) |> hd
     sender = FacebookMessenger.Response.message_senders(msg) |> hd
+    Logger.info(text)
+    FacebookMessenger.Sender.send(sender, text)
     response = String.split(text, "") |> Enum.join("\n")
     FacebookMessenger.Sender.send(sender, response)
   end
